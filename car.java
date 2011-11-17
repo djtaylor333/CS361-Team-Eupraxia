1a2a6b2b742823af2be7b455eb99ae77c9629312
zavalam <zavalam.mz@gmail.com>
2011-11-16 21:07:27 -0800

    new

--- car.java
+++ car.java
@@ -14,7 +14,72 @@
  *
  */
 
-//SO MANY COMMENTS! -James Cunnigham
-//This is a comment -Zander Clucas
-//This is also a comment too I think... -David Taylor
-//This is a coment by Marcos, Working well with eclipse
+public class car{
+	private double speed;
+	private String carName;
+	private int location;
+
+	/*first Constructor*/
+	public car( double speed, String carName, int location){
+
+		this.speed= speed;
+		this.CarName= carName;
+		this.location = location;
+	}
+
+	/*getter for car speed*/
+	public double getSpeed(){
+		return speed;
+	}
+
+	/*getter for car name*/
+	public String getCarName(){
+		return carName;
+	}
+
+	/*getter for car location*/
+	public int getLocation(){
+		return location;
+	}
+
+
+	/*setter for car speed*/
+	public void setSpeed(double spd){
+		speed=spd;
+	}
+
+	/*setter for  car name*/
+	public void setCarName(String str){
+		carName=str;
+	}
+
+	/*setter for car location*/
+	public void setLocation(int loc){
+		location= loc;
+	}
+
+	/*method to increase car speed*/
+	public double increaseSpeed(double spd){
+		if(spd<0){
+			throw new IllegalArgumentException();
+		}
+		speed = speed + spd;
+		return speed;
+	}
+
+	/*method to decrease car speed*/
+	public double decreaseSpeed(double spd){
+		if(spd<0){
+			throw new IllegalArgumentException();
+		}
+		//check for negative speed
+		if( (speed - spd) > 0){
+			speed = speed - spd;
+		}else{
+			System.out.println("Car can not have a negative a speed")
+		}
+		return speed;
+	}
+
+
+}
